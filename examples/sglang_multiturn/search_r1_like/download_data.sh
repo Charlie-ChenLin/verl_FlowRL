feba794f425935f74e6db5e1a15125d336b2ad06
LinChen——Macbook Air <lchen>
2025-11-24 11:58:46 +0800

    update retrieve data save path

--- examples/sglang_multiturn/search_r1_like/download_data.sh
+++ examples/sglang_multiturn/search_r1_like/download_data.sh
@@ -1,3 +1,4 @@
-mkdir -p ~/data/search_r1_retriever
-python examples/sglang_multiturn/search_r1_like/local_dense_retriever/download.py --save_path ~/data/search_r1_retriever
-cat ~/data/search_r1_retriever/part_a* > ~/data/search_r1_retriever/e5_Flat.index  # 把分片拼成索引
+mkdir -p ./data/search_r1_retriever
+python examples/sglang_multiturn/search_r1_like/local_dense_retriever/download.py \
+  --save_path ./data/search_r1_retriever
+cat ./data/search_r1_retriever/part_a* > ./data/search_r1_retriever/e5_Flat.index
